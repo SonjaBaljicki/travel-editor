5be60c8a0fe6aaa0ec3a144b919b7fe2748100e9
Sonja Baljicki <sonjabaljicki2002@gmail.com>
2024-10-18 14:40:19 +0200

    adding commands for deleting entities

---
+++ TravelEditor/Commands/DeleteAttractionCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using TravelEditor.Models;
+using TravelEditor.ViewModels;
+
+namespace TravelEditor.Commands
+{
+    internal class DeleteAttractionCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+        public MainViewModel viewModel;
+
+        public DeleteAttractionCommand(MainViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+            this.viewModel.PropertyChanged += (sender, e) =>
+            {
+                if (e.PropertyName == nameof(MainViewModel.SelectedAttraction))
+                {
+                    CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+                }
+            };
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return viewModel.SelectedAttraction!=null;
+        }
+
+        public void Execute(object? parameter)
+        {
+            Attraction attraction = (Attraction)viewModel.SelectedAttraction;
+            if(attraction != null)
+            {
+                MessageBox.Show(attraction.Name);
+
+            }
+            else
+            {
+                MessageBox.Show("Please select the attraction");
+            }
+        }
+    }
+}
--- TravelEditor/ViewModels/MainViewModel.cs
+++ TravelEditor/ViewModels/MainViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using TravelEditor.Commands;
 using TravelEditor.Database;
 using TravelEditor.Models;
 using TravelEditor.Services;
@@ -19,11 +22,75 @@ namespace TravelEditor.ViewModels
         private readonly IReviewService _reviewService;
         private readonly ITravellerService _travellerService;
 
+        private Trip? _selectedTrip;
+        public Trip? SelectedTrip
+        {
+            get { return _selectedTrip; }
+            set
+            {
+                _selectedTrip = value;
+                OnPropertyChanged(nameof(SelectedTrip));
+            }
+        }
+        private Destination? _selectedDestination;
+        public Destination? SelectedDestination
+        {
+            get { return _selectedDestination; }
+            set
+            {
+                _selectedDestination = value;
+                OnPropertyChanged(nameof(SelectedDestination));
+            }
+        }
+        private Attraction? _selectedAttraction;
+        public Attraction? SelectedAttraction
+        {
+            get { return _selectedAttraction; }
+            set
+            {
+                _selectedAttraction = value;
+                OnPropertyChanged(nameof(SelectedAttraction));
+            }
+        }
+        private Traveller? _selectedTraveller;
+        public Traveller? SelectedTraveller
+        {
+            get { return _selectedTraveller; }
+            set
+            {
+                _selectedTraveller = value;
+                OnPropertyChanged(nameof(SelectedTraveller));
+            }
+        }
+        private Review? _selectedReview;
+        public Review? SelectedReview
+        {
+            get { return _selectedReview; }
+            set
+            {
+                _selectedReview = value;
+                OnPropertyChanged(nameof(SelectedReview));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
         public ObservableCollection<Trip> Trips { get; set; }
         public ObservableCollection<Destination> Destinations { get; set; }
         public ObservableCollection<Attraction> Attractions { get; set; }
         public ObservableCollection<Review> Reviews { get; set; }
         public ObservableCollection<Traveller> Travellers { get; set; }
+
+        public DeleteTripCommand DeleteTripCommand { get; }
+        public DeleteDestinationCommand DeleteDestinationCommand { get; }
+        public DeleteAttractionCommand DeleteAttractionCommand { get; set; }
+        public DeleteTravellerCommand DeleteTravellerCommand { get; }
+        public DeleteReviewCommand DeleteReviewCommand { get; }
 
         public MainViewModel(ITripService tripService, IDestinationService destinationService, IAttractionService attractionService, IReviewService reviewService, ITravellerService travellerService)
         {
@@ -33,6 +100,11 @@ namespace TravelEditor.ViewModels
             _reviewService = reviewService;
             _travellerService = travellerService;
             LoadData();
+            DeleteTripCommand = new DeleteTripCommand(this);
+            DeleteDestinationCommand = new DeleteDestinationCommand(this);
+            DeleteAttractionCommand = new DeleteAttractionCommand(this);
+            DeleteTravellerCommand = new DeleteTravellerCommand(this);
+            DeleteReviewCommand = new DeleteReviewCommand(this);
 
         }
 
